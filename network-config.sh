fde91bc6dbc77ad13108f56950da52c579ec4bc6
Adi Enzel <adienzel@gmail.com>
2024-11-20 12:17:26 +0200

    add configurable interface value

--- network-config.sh
+++ network-config.sh
@@ -4,10 +4,11 @@
 PARTIAL_ADDRESS=${WSS_PARTIAL_ADDRESS:-"192.168.1"}
 START=${WSS_START_ADDRESS:-2}
 STOP=${WSS_END_ADDRESS:-63}
+INTERFACE=${WSS_INTERFACE_TO_USE:-"lo"}
 
 # Add IP addresses to the network interface
 for i in $(seq $START $STOP); do
-    ip addr add $PARTIAL_ADDRESS.$i/24 dev eth0
+    ip addr add $PARTIAL_ADDRESS.$i/24 dev $INTERFACE
 done
 
 # Start the application
