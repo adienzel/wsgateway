0f154baab5d2e09f6486ce7a874f5921aa346b8d
Adi Enzel <adienzel@gmail.com>
2025-04-15 11:20:05 -0400

    remove a log line

--- src/AppComponent.h
+++ src/AppComponent.h
@@ -66,7 +66,6 @@ public:
     OATPP_CREATE_COMPONENT(std::shared_ptr<oatpp::async::Executor>, executor)([] {
         OATPP_COMPONENT(std::shared_ptr<Config>, m_cmdArgs);
         //OATPP_LOGd(__func__, " {}", __LINE__)
-        OATPP_LOGd(__func__, "certificate file name =", m_cmdArgs->server_cert_filename);
 
         return std::make_shared<oatpp::async::Executor>(
                 m_cmdArgs->number_of_worker_threads, /* Data-Processing threads */
