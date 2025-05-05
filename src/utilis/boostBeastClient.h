c4bab8a4c6efefc51a7e1dad68c98fc1ad6609a5
Adi Enzel <adienzel@gmail.com>
2025-05-05 09:57:24 -0400

    reduce printing

--- src/utilis/boostBeastClient.h
+++ src/utilis/boostBeastClient.h
@@ -80,7 +80,7 @@ static boost::asio::awaitable<std::string> asyncHttpClient(std::shared_ptr<std::
     
         co_return result;
     } catch (const std::exception& e) {
-        OATPP_LOGe(__func__ , " Line %d Exception: {}", __LINE__, e.what());
+        OATPP_LOGe(__func__ , " Line {} Exception: {} \n on message {}", __LINE__, e.what(), *msg);
         co_return "error";
     
     }
