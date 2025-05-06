bf823da9726aa455864472f6f2d7ce9c447e55da
Adi Enzel <adienzel@gmail.com>
2025-05-06 04:49:53 -0400

    change scylladb script to support --network host

--- rm_scylla.sh
+++ rm_scylla.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Prefix for the container names
-PREFIX="scylla-node"
+PREFIX="scylla_node"
 
 # Stop containers with the specified prefix
 docker ps --filter "name=${PREFIX}" -q | xargs -r docker stop
