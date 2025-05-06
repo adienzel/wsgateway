4e503251e07728ab4cbe87148956a433ed38cfd6
Adi Enzel <adienzel@gmail.com>
2025-05-06 07:43:43 -0400

    time test

--- src/websocket/WebSocketListener.cpp
+++ src/websocket/WebSocketListener.cpp
@@ -113,10 +113,10 @@ oatpp::async::CoroutineStarter WebSocketListener::readMessage(const std::shared_
                         std::istringstream(str) >> ns2;
 
                         auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-                        OATPP_LOGd(__func__ , "result before send {}", ns - ns2)
+                        OATPP_LOGd(__func__ , "result before send {}", (ns - ns2)/1000000)
                         executor->execute<SendMessageCoroutine>(socket, result);
                         ns = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-                        OATPP_LOGd(__func__ , "result before after {}", ns - ns2)
+                        OATPP_LOGd(__func__ , "result before after {}", (ns - ns2)/1000000)
                         //socket->sendOneFrameTextAsync(result);
     
 //                        OATPP_LOGd(__func__, "boost::asio::co_spawn line {} result = {}", __LINE__, result);
